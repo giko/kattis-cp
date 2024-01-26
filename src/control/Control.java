910bd7cbcc8859f28c58fc71c16f48cbc61f4001
Nikita Chudakov <gikon27@proton.me>
2024-01-27 00:08:19 +0200

    Control

---
+++ src/control/Control.java
@@ -0,0 +1,178 @@
+package control;
+
+import java.io.*;
+import java.util.*;
+
+class Control {
+    public static void main(String[] args) {
+        FastIO io = new FastIO();
+        UnionFind uf = new UnionFind(500_001);
+
+        int N = io.nextInt();
+        int result = 0;
+        outer:
+        for (int i = 0; i < N; i++) {
+            int ingredientsCount = io.nextInt();
+            HashMap<Integer, Integer> setTimes = new HashMap<>();
+            for (int j = 0; j < ingredientsCount; j++) {
+                setTimes.merge(uf.findSet(io.nextInt()), 1, Integer::sum);
+            }
+            if (setTimes.isEmpty()) {
+                continue;
+            }
+            for (Map.Entry<Integer, Integer> e : setTimes.entrySet()) {
+                if (uf.sizeOfSet(e.getKey()) != e.getValue()) {
+                    continue outer;
+                }
+            }
+            Iterator<Integer> setsI = setTimes.keySet().iterator();
+            int firstSet = setsI.next();
+            while (setsI.hasNext()) {
+                uf.unionSet(firstSet, setsI.next());
+            }
+
+            ++result;
+        }
+        io.println(result);
+        io.close();
+    }
+}
+
+class UnionFind {                                              // OOP style
+    private ArrayList<Integer> p, rank, setSize;
+    private int numSets;
+
+    public UnionFind(int N) {
+        p = new ArrayList<>(N);
+        rank = new ArrayList<>(N);
+        setSize = new ArrayList<>(N);
+        numSets = N;
+        for (int i = 0; i < N; i++) {
+            p.add(i);
+            rank.add(0);
+            setSize.add(1);
+        }
+    }
+
+    public int findSet(int i) {
+        if (p.get(i) == i) return i;
+        else {
+            int ret = findSet(p.get(i));
+            p.set(i, ret);
+            return ret;
+        }
+    }
+
+    public Boolean isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+
+    public void unionSet(int i, int j) {
+        if (!isSameSet(i, j)) {
+            numSets--;
+            int x = findSet(i), y = findSet(j);
+            // rank is used to keep the tree short
+            if (rank.get(x) > rank.get(y)) {
+                p.set(y, x);
+                setSize.set(x, setSize.get(x) + setSize.get(y));
+            } else {
+                p.set(x, y);
+                setSize.set(y, setSize.get(y) + setSize.get(x));
+                if (rank.get(x) == rank.get(y)) rank.set(y, rank.get(y) + 1);
+            }
+        }
+    }
+
+    public int numDisjointSets() {
+        return numSets;
+    }
+
+    public int sizeOfSet(int i) {
+        return setSize.get(findSet(i));
+    }
+}
+
+class FastIO extends PrintWriter {
+    private InputStream stream;
+    private byte[] buf = new byte[1 << 16];
+    private int curChar;
+    private int numChars;
+
+    // standard input
+    public FastIO() {
+        this(System.in, System.out);
+    }
+
+    public FastIO(InputStream i, OutputStream o) {
+        super(o);
+        stream = i;
+    }
+
+    // file input
+    public FastIO(String i, String o) throws IOException {
+        super(new FileWriter(o));
+        stream = new FileInputStream(i);
+    }
+
+    // throws InputMismatchException() if previously detected end of file
+    private int nextByte() {
+        if (numChars == -1) {
+            throw new InputMismatchException();
+        }
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new InputMismatchException();
+            }
+            if (numChars == -1) {
+                return -1;  // end of file
+            }
+        }
+        return buf[curChar++];
+    }
+
+    // to read in entire lines, replace c <= ' '
+    // with a function that checks whether c is a line break
+    public String next() {
+        int c;
+        do {
+            c = nextByte();
+        } while (c <= ' ');
+
+        StringBuilder res = new StringBuilder();
+        do {
+            res.appendCodePoint(c);
+            c = nextByte();
+        } while (c > ' ');
+        return res.toString();
+    }
+
+    public int nextInt() {  // nextLong() would be implemented similarly
+        int c;
+        do {
+            c = nextByte();
+        } while (c <= ' ');
+
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = nextByte();
+        }
+
+        int res = 0;
+        do {
+            if (c < '0' || c > '9') {
+                throw new InputMismatchException();
+            }
+            res = 10 * res + c - '0';
+            c = nextByte();
+        } while (c > ' ');
+        return res * sgn;
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+}
